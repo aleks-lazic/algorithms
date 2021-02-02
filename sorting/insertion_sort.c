23592dc2431c75d15d9edd4a172c42969744aee4
aleks-lazic <aleksandar.lazic.1995@gmail.com>
2021-02-02 18:08:08 +0100

    insertion_sort

--- sorting/insertion_sort.c
+++ sorting/insertion_sort.c
@@ -4,24 +4,25 @@
 /*
 Imagine you have some cards in your hand and another pile on the table
 whenever you pick a card from the table (the key), 
-you shift the card on the left until the cards before are bigger
+you shift the cards bigger than the key on the right until you find
+the appropriate place for the card
 */
 int *insertion_sort (int *input, int size){
     int key, j = 0;
     for (int i = 1 ; i<size ; i++){
         key = input[i]; 
-        j = i;
-        while(input[j-1] > key && j > 0){
-            input[j] = input[j-1];
-            input[j-1] = key;
+        j = i -1;
+        while(input[j] > key && j >= 0){
+            input[j+1] = input[j];
             j--;
         }
+        input[j+1] = key;
     }
     return input;
 }
 
 int main () {
-    int size = 5;
+    int size = 10;
     int *a = generate_int_array(size);
     print_array(a, size);
     insertion_sort(a, size);
